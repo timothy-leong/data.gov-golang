12ae96f8969bb5fa82db1008d38672964805d74b
Timothy Leong <timothy.leong@u.nus.edu>
2022-08-20 10:54:21 +0800

    Abstract caching logic

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -30,15 +30,31 @@ func NewDataGovClient() *DataGovClient {
 	}
 }
 
+func (d *DataGovClient) refreshRate(key string) (rate time.Duration) {
+	switch key {
+	case endpoints.CarparkAvailability:
+		return time.Minute
+	}
+	return
+}
+
+func (d *DataGovClient) checkPreviousValue(key string, t time.Time) (prevValue any, exists bool) {
+	if value, ok := d.cache[key]; ok {
+		switch key {
+		case endpoints.CarparkAvailability:
+			lastFetchedValue := value.(apiobjects.CarparkAvailability)
+			if lastFetchedValue.Items[0].Timestamp.Add(d.refreshRate(endpoints.CarparkAvailability)).After(t) {
+				return value, true
+			}
+		}
+	}
+	return
+}
+
 func (d *DataGovClient) CarparkAvailability(t time.Time) (*apiobjects.CarparkAvailability, error) {
 	// Check cache for a previously fetched value
-	if value, ok := d.cache[endpoints.CarparkAvailability]; ok {
-		lastFetchedValue := value.(apiobjects.CarparkAvailability)
-		const refreshRate = time.Minute
-
-		if lastFetchedValue.Items[0].Timestamp.Add(refreshRate).After(t) {
-			return &lastFetchedValue, nil
-		}
+	if value, ok := d.checkPreviousValue(endpoints.CarparkAvailability, t); ok {
+		return value.(*apiobjects.CarparkAvailability), nil
 	}
 
 	statusCode, body, err := fasthttp.Get([]byte{}, endpoints.CarparkAvailability)
