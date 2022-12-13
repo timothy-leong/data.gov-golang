df12e5e4baf1665e872866502d446de407ea1202
Timothy Leong <timothy.leong@u.nus.edu>
2022-12-14 00:32:19 +0800

    Add test

--- pkg/client/client_test.go
+++ pkg/client/client_test.go
@@ -34,3 +34,17 @@ func TestTrafficImages(t *testing.T) {
 		t.Errorf("Client cache is empty but the result should have been cached")
 	}
 }
+
+func TestLatestUvIndexReadings(t *testing.T) {
+	client := NewDataGovClient()
+	_, err := client.LatestUVReadings(time.Now())
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check that this result has been cached
+	if _, ok := client.cache[endpoints.UvIndex]; !ok {
+		t.Errorf("Client cache is empty but the result should have been cached")
+	}
+}
